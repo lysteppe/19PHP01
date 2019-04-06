9aa347dd7cfecb59ab6c835cfa28726b30dd1f71
Nguyen <nguyenkooldn123@gmail.com>
2019-04-06 18:21:40 +0700

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,12 @@
+function chia() {
+  document.calculator.display.value +='/';
+}
+function nhan() {
+  document.calculator.display.value +='*';
+}
+function tru() {
+  document.calculator.display.value +='-';
+}
+function cong() {
+  document.calculator.display.value +='+';
+}
